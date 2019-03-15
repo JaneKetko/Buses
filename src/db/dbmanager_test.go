b24526b2d7d97d4e3c6d4e62345926141bce66ae
JaneKetko <janeketko@jmail.com>
2019-03-15 12:15:50 +0300

    test database

--- src/db/dbmanager_test.go
+++ src/db/dbmanager_test.go
@@ -1,5 +1,3 @@
-// +build integration
-
 package dbmanager
 
 import (
@@ -30,50 +28,109 @@ func DBOpen() (*sql.DB, error) {
 	return db, nil
 }
 
-func TestInsertDeletePoint(t *testing.T) {
-
-	db, err := DBOpen()
-	assert.NoError(t, err)
-
-	id, err := insertPoint(db, "Minsk", "Vitebsk")
-	assert.NoError(t, err)
-
-	_, err = db.Exec("DELETE FROM points where id_points=?", id)
-	assert.NoError(t, err)
-}
-
-func TestInsertRoute(t *testing.T) {
-
-	db, err := DBOpen()
-	assert.NoError(t, err)
-
-	id, err := insertRoute(db, 7, 32, 44, "2019-02-24 08:30:00", 15.2)
-	assert.NoError(t, err)
-
-	_, err = db.Exec("DELETE FROM route where id_route=?", id)
-	assert.NoError(t, err)
-}
-
 func TestRouteID(t *testing.T) {
 
 	db, err := DBOpen()
-	if err != nil {
-		t.Errorf("error with opening testdb: %s", err)
-	}
+	assert.NoError(t, err)
 
-	id, err := insertRoute(db, 7, 32, 44, "2019-02-24 08:30:00", 15.2)
-	if err != nil {
-		t.Errorf("error was not expected: %s", err)
-	}
+	id1, err := insertRoute(db, 7, 32, 44, "2019-02-24 08:30:00", 15.2)
+	assert.NoError(t, err)
+	id2, err := insertRoute(db, 7, 32, 44, "02-24 08:30:00", 15.2)
+	assert.NoError(t, err)
 
 	dbmanager := NewDBManager(db)
-	_, err = dbmanager.RouteByID(int(id))
-	if err != nil {
-		t.Errorf("error was not expected: %s", err)
-	}
+	_, err = dbmanager.RouteByID(int(id1))
+	assert.NoError(t, err)
+	_, err = dbmanager.RouteByID(int(id2))
+	assert.EqualError(t, err, "errors with types")
+
+	_, err = db.Exec("DELETE FROM route where id_route=?", id1)
+	assert.NoError(t, err)
+	_, err = db.Exec("DELETE FROM route where id_route=?", id2)
+	assert.NoError(t, err)
+}
+
+func TestAddRoute(t *testing.T) {
+
+	db, err := DBOpen()
+	assert.NoError(t, err)
+	dbmanager := NewDBManager(db)
+
+	id, err := dbmanager.AddRoute("Minsk", "Vitebsk", "2019-02-24 08:30:00", 15.2, 32, 44)
+	assert.NoError(t, err)
 
 	_, err = db.Exec("DELETE FROM route where id_route=?", id)
-	if err != nil {
-		t.Errorf("errors with deleting row: %s", err)
-	}
+	assert.NoError(t, err)
+
+	id, err = dbmanager.AddRoute("Minsk", "Lida", "2019-02-24 08:30:00", 15.2, 32, 44)
+	assert.NoError(t, err)
+	_, err = db.Exec("DELETE FROM points where startpoint=? && endpoint=?", "Minsk", "Lida")
+	assert.NoError(t, err)
+
+	_, err = db.Exec("DELETE FROM route where id_route=?", id)
+	assert.NoError(t, err)
+}
+
+func TestGetAllData(t *testing.T) {
+	db, err := DBOpen()
+	assert.NoError(t, err)
+	dbmanager := NewDBManager(db)
+
+	id1, err := dbmanager.AddRoute("Minsk", "Vitebsk", "2019-02-24 08:30:00", 15.2, 32, 44)
+	assert.NoError(t, err)
+	id2, err := dbmanager.AddRoute("Minsk", "Vitebsk", "2019-02-24 08:30:00", 15.2, 32, 44)
+	assert.NoError(t, err)
+	id3, err := dbmanager.AddRoute("Minsk", "Vitebsk", "2019-02-24 08:30:00", 15.2, 32, 44)
+	assert.NoError(t, err)
+
+	routes, err := dbmanager.GetAllData()
+	assert.NoError(t, err)
+
+	assert.Equal(t, 3, len(routes))
+
+	_, err = db.Exec("DELETE FROM route where id_route=?", id1)
+	assert.NoError(t, err)
+	_, err = db.Exec("DELETE FROM route where id_route=?", id2)
+	assert.NoError(t, err)
+	_, err = db.Exec("DELETE FROM route where id_route=?", id3)
+	assert.NoError(t, err)
+}
+
+func TestDeleteRoute(t *testing.T) {
+	db, err := DBOpen()
+	assert.NoError(t, err)
+	dbmanager := NewDBManager(db)
+
+	id, err := dbmanager.AddRoute("Minsk", "Vitebsk", "2019-02-24 08:30:00", 15.2, 32, 44)
+	assert.NoError(t, err)
+	err = dbmanager.DeleteRow(id)
+	assert.NoError(t, err)
+
+	_, err = dbmanager.RouteByID(id)
+
+	assert.EqualError(t, err, "no such route")
+}
+
+func TestFindRoute(t *testing.T) {
+	db, err := DBOpen()
+	assert.NoError(t, err)
+	dbmanager := NewDBManager(db)
+
+	id1, err := dbmanager.AddRoute("Minsk", "Gomel", "2019-02-24 08:30:00", 15.2, 32, 44)
+	assert.NoError(t, err)
+	id2, err := dbmanager.AddRoute("Minsk", "Gomel", "2019-02-24 08:30:00", 15.2, 32, 44)
+	assert.NoError(t, err)
+	id3, err := dbmanager.AddRoute("Minsk", "Lida", "2019-02-24 08:30:00", 15.2, 32, 44)
+	assert.NoError(t, err)
+
+	routes, err := dbmanager.FindRoute("Gomel")
+	assert.NoError(t, err)
+
+	assert.Equal(t, 2, len(routes))
+	_, err = db.Exec("DELETE FROM route where id_route=?", id1)
+	assert.NoError(t, err)
+	_, err = db.Exec("DELETE FROM route where id_route=?", id2)
+	assert.NoError(t, err)
+	_, err = db.Exec("DELETE FROM route where id_route=?", id3)
+	assert.NoError(t, err)
 }
